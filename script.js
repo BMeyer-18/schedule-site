28bd41863e9a67b844a57e7af86f68ce64f4bea9
Ben <bzmeyer18@gmail.com>
2026-06-29 22:16:00 -0400

    form elements and full table added

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-"use strict";
+const test = document.getElementById("test");
+const grid = document.getElementById("container");
+for (let i = 0; i < 336; i++) {
+    addTile(grid);
+}
+
+function addTile(grid) {
+    const newTile = document.createElement("div");
+    newTile.addEventListener("mouseover", event => selectTile(event, newTile));
+    newTile.addEventListener("mousedown", event => selectTile(event, newTile));
+    newTile.addEventListener("mouseout", () => {
+        newTile.classList.remove("hovering");
+    });
+    grid.appendChild(newTile);
+}
+
+function selectTile(event, tile) {
+    if (event.buttons > 0) {
+        tile.classList.toggle("selected");
+    } else if (!tile.classList.contains("selected")) {
+        tile.classList.add("hovering");
+    }
+    test.innerHTML = tile.style.backgroundColor;
+}
