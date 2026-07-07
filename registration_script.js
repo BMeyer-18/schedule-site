18b7710476518850db8447023dbdd6510fe776b0
Ben <bzmeyer18@gmail.com>
2026-07-07 19:46:13 -0400

    event creation page now has a functional delete button

--- registration_script.js
+++ registration_script.js
@@ -26,3 +26,32 @@ async function submitEvent() {
         message.innerHTML = "FAILURE: " + responseObj.info;
     }
 }
+
+async function deleteEvent() {
+    const event = document.getElementById("event").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    if (event.length === 0 || password.length === 0) {
+        message.innerHTML = "Please enter both an event and a password";
+        return;
+    }
+
+    const response = await fetch(`http://localhost:3000/api/v1/passwords/${event}`, {
+        method: "DELETE",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            password: password
+        })
+    });
+    const responseObj = await response.json();
+
+    if (response.status === 200) {
+        message.innerHTML = "SUCCESS: " + responseObj.info;
+    } else {
+        message.innerHTML = "FAILURE: " + responseObj.info;
+    }
+}
