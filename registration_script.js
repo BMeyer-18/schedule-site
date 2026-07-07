4b640729ef4a4ffe0dd246a706a16976734539ff
Ben <bzmeyer18@gmail.com>
2026-07-07 18:54:52 -0400

    added page for creating new events w/ passwords, fully functional

---
+++ registration_script.js
@@ -0,0 +1,28 @@
+async function submitEvent() {
+    const event = document.getElementById("event").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    if (event.length === 0 || password.length === 0) {
+        message.innerHTML = "Please enter both an event and a password";
+        return;
+    }
+
+    const response = await fetch(`http://localhost:3000/api/v1/passwords/${event}`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            password: password
+        })
+    });
+    const responseObj = await response.json();
+
+    if (response.status === 201) {
+        message.innerHTML = "SUCCESS: " + responseObj.info;
+    } else {
+        message.innerHTML = "FAILURE: " + responseObj.info;
+    }
+}
